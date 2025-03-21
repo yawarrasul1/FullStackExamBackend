dc13eb8e320c9cec7fef43bf361f9bcc022d37e5
yawarrasul1 <98248173+yawarrasul1@users.noreply.github.com>
2025-03-21 15:09:18 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const ProductController = require("../controllers/ProductController");
+const ProductController = require("../controllers/productController");
 const { authenticate } = require("../middleware/authMiddleware");
 
 // Public routes
